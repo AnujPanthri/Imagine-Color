b870f42071c9579ffbab98735e35df5b51968109
Ankur Panthri <panthrianuj@gmail.com>
2023-12-15 21:06:34 +0530

    added notifications

--- DjangoWeb/main/static/main/index.js
+++ DjangoWeb/main/static/main/index.js
@@ -15,7 +15,10 @@ function createColorCard(colorname,color){
     card.dataset.color=color;
     card.innerHTML=`
         <div class="card-color" style='background:${color}'>
-            <i class="fa-solid fa-copy card-copy-btn" onclick="copyToClipboard(this.closest('.card').dataset.color)"></i>
+            <i class="fa-solid fa-copy card-copy-btn"
+             onclick="copyToClipboard(this.closest('.card').dataset.color);addNotification('Copied !','success');"
+
+             ></i>
             <i class="fa-solid fa-heart card-like-btn"></i>
         </div>
         <div class="card-title">${colorname}</div>
@@ -63,3 +66,28 @@ function detectOnSubmit(e){
     if(e.key==='Enter' || e.keyCode===13)
         generateColor()
 }
+
+
+function addNotification(msg,type){
+    // <div class="toast">
+    //     <i class="fa-solid fa-check"></i>
+    //     <span>copied!</span>
+    // </div>
+
+    var toast = document.createElement("div");
+    toast.className = 'toast';
+    if(type=="success"){
+        toast.innerHTML=`<i class="fa-solid fa-check"></i>`;
+    }
+    else if(type=="error"){
+        toast.innerHTML=`<i class="fa-solid fa-cross"></i>`;
+    }
+    toast.innerHTML+=`<span>${msg}</span>`;
+    toast_container.append(toast);
+
+    setTimeout(()=>{
+        toast.remove();
+    },1000*2);
+}
+
+// addNotification('msg','success');
